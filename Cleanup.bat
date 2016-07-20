e1781c5a52f8674f57a0044b7e716dfa925ee430
Ivan Kuznetsov <ivandevel@gmail.com>
2016-07-20 12:04:00 +0500

    - minor changes

--- Cleanup.bat
+++ Cleanup.bat
@@ -1,8 +1,12 @@
 del *.ldb /F /Q
 del *.laccdb /S /F /Q
 
-RMDIR History /S /Q
-RMDIR SchLib\History /S /Q
-RMDIR PcbLib\History /S /Q
+# RMDIR History /S /Q
+# RMDIR SchLib\History /S /Q
+# RMDIR PcbLib\History /S /Q
+
+for /d /r . %%d in (History) do @if exist "%%d" echo "%%d" && rd /s/q "%%d"
+
+for /d /r . %%d in (__Previews) do @if exist "%%d" echo "%%d" && rd /s/q "%%d"
 
 sleep 3
